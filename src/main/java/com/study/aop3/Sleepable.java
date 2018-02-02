e5b50cf1674febf57fd8fd8512d9a767cb13a188
guodd <guodongdong@aicaigroup.com>
2018-02-02 17:40:53 +0800

    提交

--- src/main/java/com/study/aop3/Sleepable.java
+++
@@ -1,8 +0,0 @@
-package com.study.aop3;
-
-/**
- * Created by guodd on 2018/1/29.
- */
-public interface Sleepable {
-    void sleep();
-}
---
+++ src/main/java/com/study/proxy/cglib/proxy1/Programmer.java
@@ -0,0 +1,11 @@
+package com.study.proxy.cglib.proxy1;
+
+/**
+ * Created by guodd on 2018/2/1.
+ */
+public class Programmer {
+
+    public void code(){
+        System.out.println("I'm a Programmer, Just Coding ...");
+    }
+}
